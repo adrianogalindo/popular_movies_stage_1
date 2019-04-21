7608a33e70d3f9ae77c951a1408d59f732aa2ff9
Adriano Galindo <adrianogalindo.git@gmail.com>
2019-04-21 12:15:12 -0400

    Add RecyclerView widget to activity_main.xml and references to MainActivity.java

--- app/src/main/java/com/galindo/popular_movies_stage_1/MainActivity.java
+++ app/src/main/java/com/galindo/popular_movies_stage_1/MainActivity.java
@@ -2,12 +2,17 @@ package com.galindo.popular_movies_stage_1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mMainActivityRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mMainActivityRecyclerView = findViewById(R.id.rv_main_activity);
     }
 }
